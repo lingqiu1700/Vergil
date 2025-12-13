5e87a8241a8a8752bf3ffaf3e97e59c55e5245eb
lingqiu <2700067374@qq.com>
2025-12-14 05:52:19 +0800

    skills

---
+++ src/components/sklls_description.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <div class="global">
+        <div class="skills-description">
+            <div v-for="(item, index) in sklls" :key="index" class="skills">
+                <h2>{{ item.sklls_name }}</h2>
+                <p>{{ item.sklls_description }}</p>
+                <p><strong>指令:</strong> {{ item.sklls_command }}</p>
+                <img :src="item.img"></img>
+                <hr />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SkllsDescription',
+    props: {
+        sklls: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style src="/src/styles/sklls_description.css"></style>
