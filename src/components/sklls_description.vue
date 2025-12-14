418a565d723cfc4c7b350e3077c9887eecf2cc2f
lingqiu <2700067374@qq.com>
2025-12-14 15:50:01 +0800

    1.0

--- src/components/sklls_description.vue
+++ src/components/sklls_description.vue
@@ -5,8 +5,8 @@
             <div v-for="(item, index) in sklls" :key="index" class="skills">
                 <h2>{{ item.sklls_name }}</h2>
                 <p>{{ item.sklls_description }}</p>
-                <p><strong>指令:</strong> {{ item.sklls_command }}</p>
                 <img :src="item.img"></img>
+                <p><strong>指令:</strong> {{ item.sklls_command }}</p>
                 <hr />
             </div>
         </div>
